c15ba07b42d3472773b6a83f6ce04af3c051e986
Aidan Feldman <aidan.feldman@gmail.com>
2013-10-20 21:20:43 -0400

    stub more tests for git-github-repo

--- spec/git_github_repo_spec.rb
+++ spec/git_github_repo_spec.rb
@@ -15,4 +15,8 @@ describe 'git-github-repo' do
     `git remote add origin https://github.com/afeld/git-plugins-test`
     `git github-repo`.should eq("afeld/git-plugins-test\n")
   end
+
+  it "fails for paths"
+
+  it "fails for non-github git:// remotes"
 end
