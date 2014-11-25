f5052c9435c33dab67cbd4c0b30295d456486108
Aidan Feldman <aidan.feldman@gsa.gov>
2014-11-25 02:27:55 -0600

    refactor primary branch spec setup

--- spec/git_primary_branch_spec.rb
+++ spec/git_primary_branch_spec.rb
@@ -1,9 +1,38 @@
 describe 'git-primary-branch' do
   describe "with a remote" do
-    def set_up_remote
+    def to_branch_list(branches_str)
+      branches_str.split(/\n/).map(&:strip).sort
+    end
+
+    def branches
+      to_branch_list(execute('git branch'))
+    end
+
+    def remote_branches
+      to_branch_list(execute('git branch -r'))
+    end
+
+    def set_up_remote(primary: 'master', secondary: nil)
+      Dir.chdir('remote_repo') do
+        `git commit --allow-empty -m "initial commit"`
+        `git branch -m #{primary}`
+        if secondary
+          `git branch #{secondary}`
+          expect(branches).to eq(["* #{primary}", secondary])
+        else
+          expect(branches).to eq(["* #{primary}"])
+        end
+      end
+
       `git remote add origin remote_repo`
       expect(execute('git remote')).to eq("origin\n")
       `git fetch origin`
+
+      if secondary
+        expect(remote_branches).to eq(["origin/#{primary}", "origin/#{secondary}"].sort)
+      else
+        expect(remote_branches).to eq(["origin/#{primary}"])
+      end
     end
 
     before do
@@ -12,43 +41,18 @@ describe 'git-primary-branch' do
 
     it "uses an arbitrary branch when it's the only one" do
       branch = "rand-#{rand(100)}"
-      Dir.chdir('remote_repo') do
-        `git commit --allow-empty -m "initial commit"`
-        `git branch -m #{branch}`
-        expect(execute('git branch')).to eq("* #{branch}\n")
-      end
-
-      set_up_remote
-      expect(execute('git branch -r')).to eq("  origin/#{branch}\n")
-
+      set_up_remote(primary: branch)
       expect(execute('git primary-branch')).to eq("#{branch}\n")
     end
 
     it "favors `master` over `gh-pages`" do
-      Dir.chdir('remote_repo') do
-        `git commit --allow-empty -m "initial commit"`
-        `git branch gh-pages`
-        expect(execute('git branch')).to eq("  gh-pages\n* master\n")
-      end
-
-      set_up_remote
-      expect(execute('git branch -r')).to eq("  origin/gh-pages\n  origin/master\n")
-
+      set_up_remote(secondary: 'gh-pages')
       expect(execute('git primary-branch')).to eq("master\n")
     end
 
     it "favors `gh-pages` over others" do
       branch = "rand-#{rand(100)}"
-      Dir.chdir('remote_repo') do
-        `git commit --allow-empty -m "initial commit"`
-        `git branch -m gh-pages`
-        `git branch #{branch}`
-        expect(execute('git branch')).to eq("* gh-pages\n  #{branch}\n")
-      end
-
-      set_up_remote
-      expect(execute('git branch -r')).to eq("  origin/gh-pages\n  origin/#{branch}\n")
-
+      set_up_remote(primary: 'gh-pages', secondary: branch)
       expect(execute('git primary-branch')).to eq("gh-pages\n")
     end
   end
