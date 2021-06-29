158ae4b0b4ba22dfd52bf4d4c2af6f44982cd4db
Aidan Feldman <aidan.feldman@gsa.gov>
2021-06-29 08:28:46 -0400

    simplify primary branch tests

--- spec/git_primary_branch_spec.rb
+++ spec/git_primary_branch_spec.rb
@@ -23,18 +23,20 @@ describe 'git-primary-branch' do
   end
 
   describe "with a remote" do
+    REMOTE_REPO_DIR = 'remote_repo'
+
     def remote_branches
       to_branch_list(execute('git branch -r'))
     end
 
     def create_remote(primary: 'master', secondary: nil)
-      Dir.chdir('remote_repo') do
+      Dir.chdir(REMOTE_REPO_DIR) do
         create_branches(primary: primary, secondary: secondary)
       end
     end
 
     def add_remote
-      `git remote add origin remote_repo`
+      `git remote add origin #{REMOTE_REPO_DIR}`
       expect(execute('git remote')).to eq("origin\n")
       `git fetch origin`
     end
@@ -56,53 +58,25 @@ describe 'git-primary-branch' do
       `git init remote_repo`
     end
 
-    it "uses an arbitrary branch when it's the only one" do
-      branch = random_branch_name
-      set_up_remote(primary: branch)
-      expect(execute('git primary-branch')).to eq("#{branch}\n")
-    end
-
-    it "favors `main` over `master`" do
-      set_up_remote(primary: 'main', secondary: 'master')
-      expect(execute('git primary-branch')).to eq("main\n")
-    end
-
-    it "favors `master` over `gh-pages`" do
-      set_up_remote(secondary: 'gh-pages')
+    it "uses the primary branch" do
+      set_up_remote()
       expect(execute('git primary-branch')).to eq("master\n")
     end
 
-    it "favors `gh-pages` over others" do
+    it "uses the primary branch when it's something arbitrary" do
       branch = random_branch_name
-      set_up_remote(primary: 'gh-pages', secondary: branch)
-      expect(execute('git primary-branch')).to eq("gh-pages\n")
+      set_up_remote(primary: branch, secondary: 'master')
+      expect(execute('git primary-branch')).to eq("#{branch}\n")
     end
   end
 
   describe "without a remote" do
-    it "uses an arbitrary branch when it's the only one" do
-      branch = random_branch_name
-      create_branches(primary: branch)
+    it "uses the local setting" do
+      branch = 'foo'
+      `git config init.defaultBranch #{branch}`
       expect(execute('git primary-branch')).to eq("#{branch}\n")
     end
 
-    it "favors `master` over `gh-pages`" do
-      create_branches(secondary: 'gh-pages')
-      expect(execute('git primary-branch')).to eq("master\n")
-    end
-
-    it "favors `gh-pages` over others" do
-      branch = random_branch_name
-      create_branches(primary: 'gh-pages', secondary: branch)
-      expect(execute('git primary-branch')).to eq("gh-pages\n")
-    end
-
-    it "picks the current branch if it doesn't recognize the others" do
-      branch1 = random_branch_name
-      branch2 = random_branch_name
-      create_branches(primary: branch1, secondary: branch2)
-      `git checkout #{branch2}`
-      expect(execute('git primary-branch')).to eq("#{branch2}\n")
-    end
+    # TODO test global default
   end
 end
